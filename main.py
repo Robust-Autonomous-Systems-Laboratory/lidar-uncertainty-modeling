f4696fd040b24a78562fcef244263841aa258c3a
Ian Q. Mattson <iqmattso@mtu.edu>
2026-08-10 18:06:50 -0400

    first attempt at getting minimal mistakes added to specific pages branch

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.pointcloud_target_processing.gui import LidarUncertaintyGUI
-from PyQt5 import QtWidgets
-import sys
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = LidarUncertaintyGUI()
-    window.show()
-    sys.exit(app.exec_())
